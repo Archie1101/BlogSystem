71c923c5672130c38822d464f080f69a2f3701f8
Achrie <18851989124@163.com>
2025-05-27 23:32:33 +0800

    将user从Login传入BlogMain

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,13 @@ module com.cczu.blogsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
-    opens com.cczu.blogsystem.view  to javafx.fxml;
-    exports com.cczu.blogsystem.controller;
+    opens com.cczu.blogsystem.view to javafx.fxml;
+    opens com.cczu.blogsystem.pojo to javafx.fxml;
     opens com.cczu.blogsystem.controller to javafx.fxml;
+    exports com.cczu.blogsystem.controller;
+    exports com.cczu.blogsystem.pojo;
+
 }
