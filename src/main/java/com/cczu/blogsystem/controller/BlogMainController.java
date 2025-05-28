c17e2f22573d1502c2e8caea9776c4aad0de1a97
Achrie <18851989124@163.com>
2025-05-28 13:58:04 +0800

    基本完成日志id 内容 评论展示

--- src/main/java/com/cczu/blogsystem/controller/BlogMainController.java
+++ src/main/java/com/cczu/blogsystem/controller/BlogMainController.java
@@ -1,14 +1,20 @@
 package com.cczu.blogsystem.controller;
 
 import com.cczu.blogsystem.dao.BlogDao;
+import com.cczu.blogsystem.dao.CommentDao;
 import com.cczu.blogsystem.pojo.Blog;
+import com.cczu.blogsystem.pojo.Comment;
 import com.cczu.blogsystem.pojo.User;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class BlogMainController {
     @FXML
@@ -27,7 +33,6 @@ public class BlogMainController {
         ObservableList<Blog> blogs = FXCollections.observableArrayList(blogDao.findMyBlogs(user));
         BlogView.setItems(blogs);
 
-        // 如果需要自定义列表显示，可以加cellFactory
         BlogView.setCellFactory(lv -> new ListCell<Blog>() {
             @Override
             protected void updateItem(Blog blog, boolean empty) {
@@ -36,10 +41,31 @@ public class BlogMainController {
                     setText(null);
                     setGraphic(null);
                 } else {
-                    setText(blog.getBlogTitle());  // 简单显示标题
+                    VBox vbox = new VBox();
+                    Label titleLabel = new Label("标题: " + blog.getBlogTitle());
+                    Label idLabel = new Label("ID: " + blog.getBlogId());
+                    Label contentLabel = new Label("内容: " + blog.getBlogContent());
+
+                    // 获取评论数据
+                    CommentDao commentDao = new CommentDao();
+                    List<Comment> comments = commentDao.findCommentById(blog.getBlogId());
+
+                    VBox commentBox = new VBox();
+                    commentBox.setSpacing(5); // 可选：增加评论间距
+                    for (Comment comment : comments) {
+                        Label commentLabel = new Label("评论: " + comment.getCommentContent());
+                        commentBox.getChildren().add(commentLabel);
+                    }
+
+                    vbox.setSpacing(10); // 增加整体垂直间距
+                    vbox.getChildren().addAll(titleLabel, idLabel, contentLabel, commentBox);
+
+                    setText(null);
+                    setGraphic(vbox);
                 }
             }
         });
     }
+
 }
 
