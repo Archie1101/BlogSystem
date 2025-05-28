4d9fcbe10072f91856f7004fd051d35e7db3e075
Achrie <18851989124@163.com>
2025-05-28 23:25:09 +0800

    当不存在博客类型时自动添加

--- src/main/java/com/cczu/blogsystem/dao/BlogTypeDao.java
+++ src/main/java/com/cczu/blogsystem/dao/BlogTypeDao.java
@@ -3,10 +3,7 @@ package com.cczu.blogsystem.dao;
 import com.cczu.blogsystem.pojo.BlogType;
 import com.cczu.blogsystem.util.DBConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class BlogTypeDao {
     static Connection conn = null;
@@ -16,14 +13,29 @@ public class BlogTypeDao {
     public BlogType findBlogType(String typeName) {
         try {
             conn = DBConnection.getConnection();
-            String sql = "select * from BlogType where typeName=?";
+
+            String sql = "SELECT * FROM BlogType WHERE typeName = ?";
             ps = conn.prepareStatement(sql);
             ps.setString(1, typeName);
             rs = ps.executeQuery();
+
             if (rs.next()) {
                 BlogType blogType = new BlogType();
                 blogType.setTypeId(rs.getInt("typeId"));
                 blogType.setTypeName(rs.getString("typeName"));
+                return blogType;
+            }
+
+            sql = "INSERT INTO BlogType(typeName) VALUES(?)";
+            ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, typeName);
+
+            rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int Id = rs.getInt(1);
+                BlogType blogType = new BlogType();
+                blogType.setTypeId(Id);
+                blogType.setTypeName(typeName);
                 return blogType;
             }
         } catch (SQLException e) {
